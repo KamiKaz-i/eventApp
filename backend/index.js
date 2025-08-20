edcb3bc5726d91d28dae2f644d0bb9db8a38dc8b
KamiKaz-i <kamil.kazior@gmail.com>
2025-08-20 22:29:26 +0200

    cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,9 @@ const app = express();
 //await db.sequelize.sync({ force: true });
 // await db.sequelize.getQueryInterface().dropAllTables();
 // await db.sequelize.sync({ force: true });
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://eventapp-1-nxbr.onrender.com", credentials: true })
+);
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
